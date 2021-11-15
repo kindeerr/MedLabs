bda6a3df0aa3ad77660e60262f491eb1da1618d6
Sebi <ene.sebastian.2000@gmail.com>
2021-11-16 01:37:58 +0200

    DB connection init

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,32 @@
 import express from "express";
+import { connect } from "http2";
+import { MongoClient } from "mongodb";
+
 const app = express();
+const url =
+  "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.vh7uk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Establish and verify connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to server");
+    const ceva = client.db("sample_training").collection("trips");
+    //console.log(ceva);
+    const altceva = await ceva.findOne({});
+    console.log(altceva);
+    return client.db("sample_training").collection("trips");
+    //TODO: mutat serverul unde am conexiune la db, investigat daca pica conexiunea la db ce facem
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Well done!gdbfdbfd");
